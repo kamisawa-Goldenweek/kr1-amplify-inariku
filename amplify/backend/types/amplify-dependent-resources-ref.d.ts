bda9e3e24592e8e66f710baf7bccf7fa26e58ad0
unknown <kamisawa@rayoo.jp>
2023-05-08 10:35:58 +0900

    Add AI function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,5 +15,29 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "predictions": {
+    "identifyLabels2b13c3bd": {
+      "region": "string",
+      "type": "string"
+    },
+    "identifyText04d63228": {
+      "format": "string",
+      "region": "string"
+    },
+    "interpretTextbbe0b4a1": {
+      "region": "string",
+      "type": "string"
+    },
+    "speechGenerator22392a90": {
+      "language": "string",
+      "region": "string",
+      "voice": "string"
+    },
+    "translateText540e9c31": {
+      "region": "string",
+      "sourceLang": "string",
+      "targetLang": "string"
+    }
   }
 }
--- src/components/AI.vue
+++ src/components/AI.vue
@@ -1,3 +1,53 @@
 <template>
-  <h1>AI View</h1>
+  <input id="check1" class="check" type="radio" name="ai" />
+  <label class="label" for="check1"> Translate - Amazon Translate </label>
+  <div class="content">
+    <TranslateText />
+  </div>
+
+  <input id="check2" class="check" type="radio" name="ai" />
+  <label class="label" for="check2"> Indentify Labels - Amazon Rekognition </label>
+  <div class="content">
+    <IdentifyLabels />
+  </div>
+
+  <input id="check3" class="check" type="radio" name="ai" />
+  <label class="label" for="check3"> Indentify Text - Amazon Rekognition </label>
+  <div class="content">
+    <IdentifyText />
+  </div>
+
+  <input id="check4" class="check" type="radio" name="ai" />
+  <label class="label" for="check4"> Interpret Text - Amazon Comprehend </label>
+  <div class="content">
+    <InterpretText />
+  </div>
+
+  <input id="check5" class="check" type="radio" name="ai" />
+  <label class="label" for="check5"> Text to Speech - Amazon Polly </label>
+  <div class="content">
+    <!-- 追加 1-->
+    <TextSpeech />
+  </div>
 </template>
+
+<script>
+import TranslateText from '@/components/TranslateText.vue';
+import IdentifyLabels from '@/components/IdentifyLabels.vue';
+import IdentifyText from '@/components/IdentifyText.vue';
+import InterpretText from '@/components/InterpretText.vue';
+// 追加 2
+import TextSpeech from '@/components/TextSpeech.vue';
+
+export default {
+  components: {
+    TranslateText,
+    IdentifyLabels,
+    IdentifyText,
+    InterpretText,
+    // 追加 3
+    TextSpeech,
+  },
+};
+</script>
+
